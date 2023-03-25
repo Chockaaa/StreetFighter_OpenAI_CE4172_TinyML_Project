156cba6e8aa9edd9d437a8d060c052ee33b725d4
Chockalingam Kasi <chocka1998@gmail.com>
2023-03-25 13:12:54 +0800

    configure hadoken

--- StreetFighterOpenAI.py
+++ StreetFighterOpenAI.py
@@ -11,6 +11,7 @@ obs = env.reset()
 
 #https://github.com/Camille-Gouneau/Ryuforcement
 def keyboard_controller():
+    flag = False
     if keyboard.is_pressed('left'):
         action = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
     elif keyboard.is_pressed('right'):
@@ -20,25 +21,72 @@ def keyboard_controller():
     elif keyboard.is_pressed('down'):
         action = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
     elif keyboard.is_pressed('a'):
-        action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+        action = [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]
     elif keyboard.is_pressed('s'):
         action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
     elif keyboard.is_pressed('d'):
         action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-    elif keyboard.is_pressed('q'):
-        action = [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]
-    elif keyboard.is_pressed('w'):
-        action = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0]
     elif keyboard.is_pressed('e'):
         action = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
-    elif keyboard.is_pressed('z'):
-        action = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0]
+    elif keyboard.is_pressed('space'):
+        action = [[0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0], 
+                  [0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+                  [0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1], 
+                  [0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1], 
+                  [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+                  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0],
+                  [1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+                  [1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+                  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+                  [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+                  [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+                  [1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1],
+                  [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1],
+                  [1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+                  [0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+                  [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1], 
+                  [0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+                  [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+                  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+                  [0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1],
+                  [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0], 
+                  [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0],
+                  [1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1],
+                  [1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1], 
+                  [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+                  [0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0],
+                  [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1],
+                  [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1], 
+                  [0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+                  [0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+                  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+                  [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+                  [1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+                  [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+                  [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1], 
+                  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0], 
+                  [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+                  [0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+                  [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1], 
+                  [1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0], 
+                  [1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0], 
+                  [0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0], 
+                  [0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0],
+                  [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0], 
+                  [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0],
+                  [1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0],
+                  [0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+                  [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0]]
+        flag = True
     else:
         action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    return action
+    return action,flag
 
 
+bank = []
 done = False
 for game in range(1):
     count = 0
@@ -47,20 +95,31 @@ for game in range(1):
             obs = env.reset()
             
         
-        action = env.action_space.sample()
+        #action = env.action_space.sample()
+        #flag = False
+        action,flag = keyboard_controller()
 
-        #action = keyboard_controller()
+        if flag == True:
+            for i in range(len(action)):
+                obs, reward, done, info = env.step(action[i])
 
-        
 
-        obs, reward, done, info = env.step(action)
-        if(reward>0):
-            print("Reward: ",reward, "Action: ", action)
+                # Render the game
+                env.render()
+                time.sleep(0.01)
 
-        
-        # Render the game
-        env.render()
-        time.sleep(0.01)
+        else:
+            obs, reward, done, info = env.step(action)
+            bank.append(action)
+            if(reward>0):
+                print("Reward: ",reward, "Action: ", action)
+                print(bank[-70:-20])
+                print()
+
+            # Render the game
+            env.render()
+            time.sleep(0.01)
+
         
             
 env.close()
